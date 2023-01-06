55d4f23f9fc70f0fbeb171c5d2399b4f77cb2299
Usk3210 <usk3210@gmail.com>
2023-01-06 19:33:22 +0530

    commita

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect("mongodb+srv://UMSA:UMSA@cluster0.t0zfjzv.mongodb.net/?retryWri
 .then(()=>console.log("dbconnected"))
 
 const corsOptions ={
-    origin:'https://instagramcloneapp-client-siv5.onrender.com/', 
+    origin:'http://192.168.248.9:3000 /', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
