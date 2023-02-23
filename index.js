588db3b208f68eb00d5b573e020461e774e40613
Usk3210 <usk3210@gmail.com>
2023-02-23 22:19:20 +0530

    corsupdated2ndcommit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 const jwtsecretkey = process.env.jwt_secret
 const cors = require("cors")
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
